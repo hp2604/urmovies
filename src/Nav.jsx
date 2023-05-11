f6b2931e8334fb2bbc3fade5028cbcdb7e41c77d
Harsh Prajapati <95865009+hp2604@users.noreply.github.com>
2023-05-11 19:49:03 +0530

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,33 +9,53 @@ function Nav()
     return (
         <>
        
-        <div className="p-3 mb-2 bg-warning text-dark">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <div className="collapse navbar-collapse" id="navbarNav">
-  
-          
-          
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink exact className="nav-link active" aria-current="page" to={"/"} >Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to={"/bollywood"}>Bollywood </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to={"/hollywood"}>Hollywood</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink  className="nav-link" to="https://github.com/hp2604/Urmovie.git">Git</NavLink>
-        </li>
         
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+
+  <div className="container-fluid">
+
+    <a className="navbar-brand" href="#">Umovies</a>
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+      <span className="navbar-toggler-icon"></span>
+
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <li className="nav-item">
+
+          <NavLink className="nav-link active" aria-current="page" to={"/"}>Home </NavLink>
+
+        </li>
+
+        <li className"nav-item">
+
+          <NavLink className="nav-link" to={"/bollywood"}>Bollywood</NavLink>
+
+        </li>
+
+        
+
+        <li className="nav-item">
+
+          <NavLink className="nav-link " to{"/hollywood"}>Hollywood</NavLink>
+
+        </li>
+
       </ul>
-    
+
+      
+
+
     </div>
+
   </div>
+
 </nav>
-</div>
 
         </>
         
